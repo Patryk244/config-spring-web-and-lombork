e8a6d2e26186fa3d1d78b08ad5135f413c773d95
Patryk Rejent <82150867+Patryk244@users.noreply.github.com>
2025-10-13 13:14:23 +0200

    add client for trello

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,30 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("v1/trello")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class TrelloController {
+    private final TrelloClient trelloClient;
+
+    @GetMapping("boards")
+    public void getTrelloBoards(){
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+        /*
+        trelloBoards.forEach(trelloBoardDto -> {
+            System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName().equals("Kodiila"));
+        });
+
+         */
+        trelloBoards.stream()
+                .filter(b -> b.getName().equals("Kodilla"))
+                .map(b -> b.getId() + " " + b.getName())
+                .forEach(System.out::println);
+    }
+}
