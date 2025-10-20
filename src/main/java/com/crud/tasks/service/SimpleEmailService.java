e70be06b2fde27f3f19ea55c16275b92e4224697
Patryk Rejent <82150867+Patryk244@users.noreply.github.com>
2025-10-20 11:30:40 +0200

    create sending email on certain hours

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -35,6 +35,7 @@ public class SimpleEmailService {
         }
         return null;
     }
+
     private SimpleMailMessage createMailMessage(final Mail mail) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
